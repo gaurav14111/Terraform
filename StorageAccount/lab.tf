011942a9623e7d4e91c41d77f3860dbcb137da4d
Gaurav Maheshwari <dc.gaurav@gmail.com>
2020-03-18 12:55:43 -0700

    storagecreation

---
+++ StorageAccount/lab.tf
@@ -0,0 +1,15 @@
+provider "azurerm" {
+  version = 1.38
+}
+resource "azurerm_storage_account" "lab" {
+    name    = "tfstatefile-azure"
+    resource_group_name = "Terraform"
+    location = "East US"
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+  
+}
+tags = {
+    environment = "Terraform Storage"
+    CreatedBy = "Gaurav"
+}
