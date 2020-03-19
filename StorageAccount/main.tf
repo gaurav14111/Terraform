59f965f32dc87fec4bb47c582f685ed303a64bd5
Gaurav Maheshwari <dc.gaurav@gmail.com>
2020-03-18 18:10:32 -0700

    commitmaintf

--- StorageAccount/main.tf
+++ StorageAccount/main.tf
@@ -1,13 +1,14 @@
 
-provider "azurerm" '{
+provider "azurerm"{
     version=1.38
-  
+
 }
 terraform{
     backend "azurerm"{
         resource_group_name="Terraform"
-        storage_account_name="terraformtfstatefile"
+        storage_account_name="tfstatefileterraform"
         container_name ="tfstatefile"
-        key ="terraform.tfstate"
+        key ="terraform.tfstate
+
     }
 }
